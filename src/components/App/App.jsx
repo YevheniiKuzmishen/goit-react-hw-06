c0eaf7f157d2541cd4ff86fa90ab52738fa99423
YevheniiKuzmishen <yevhenii.kuzmishen@gmail.com>
2024-08-01 15:13:18 +0200

    add ContactList

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import css from "./App.module.css";
+// import ContactForm from "../ContactForn/ContactForm";
+import ContactList from "../ContactList/ContactList";
+
+export default function App() {
+  return (
+    <div className={css.contactsBook}>
+      <h1 className={css.titleBook}>Phonebook</h1>
+      {/* <ContactForm /> */}
+      {/* <SearchBox /> */}
+      <ContactList />
+    </div>
+  );
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,28 @@
+import Contact from "../Contact/Contact";
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+import css from "./ContactList.module.css";
+
+export default function ContactList() {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter)
+  );
+
+  return (
+    <ul className={css.contactList}>
+      {filteredContacts.map((contact) => (
+        <li key={contact.id}>
+          <Contact
+            id={contact.id}
+            name={contact.name}
+            number={contact.number}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
