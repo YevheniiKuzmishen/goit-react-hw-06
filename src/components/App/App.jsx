515f4c8d3a782e0fc239366fa6dc388149cb279c
YevheniiKuzmishen <yevhenii.kuzmishen@gmail.com>
2024-08-01 23:09:50 +0200

    add SearchBox

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,14 @@
 import css from "./App.module.css";
 import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
 
 export default function App() {
   return (
     <div className={css.contactsBook}>
       <h1 className={css.titleBook}>Phonebook</h1>
       <ContactForm />
-      {/* <SearchBox /> */}
+      <SearchBox />
       <ContactList />
     </div>
   );
